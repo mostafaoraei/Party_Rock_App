bb243bee851bfdfc606eb4bed53161acebada4cc
Mostafa Oraei <oraei.mossi@gmail.com>
2017-07-08 10:46:30 +0430

    Create UI design for PartyRetoApp Main controller and webView

---
+++ Party_Rock_App/Party_Rock_App/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Party_Rock_App
+//
+//  Created by Mostafa Oraei on 4/17/1396 AP.
+//  Copyright © 1396 Mostafa Oraei. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

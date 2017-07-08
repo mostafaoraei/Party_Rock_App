02c4ceb1906ed0cd66f93f310df9d264e7b24e0b
Mostafa Oraei <oraei.mossi@gmail.com>
2017-07-08 17:34:51 +0430

    WebView completed segue completed

--- Party_Rock_App/Party_Rock_App/VideoVC.swift
+++ Party_Rock_App/Party_Rock_App/VideoVC.swift
@@ -10,26 +10,25 @@ import UIKit
 
 class VideoVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var webView : UIWebView!
+    @IBOutlet weak var videoTitle : UILabel!
+    
+    private var _partyRock : PartyRock!
+    
+    var partyRock : PartyRock {
+        get {
+            return _partyRock
+        } set {
+            _partyRock = newValue
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        videoTitle.text = partyRock.videoTitle
+        webView.loadHTMLString(partyRock.videoURL, baseURL: nil)
+       
     }
-    */
 
 }
